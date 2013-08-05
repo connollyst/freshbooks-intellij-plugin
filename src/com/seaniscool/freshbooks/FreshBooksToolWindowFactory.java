1e5bd077e5809be3ad390c6447cf77ed144e3952
Sean Connolly <connolly.st@gmail.com>
2013-08-04 22:04:43 -0400

    added a swing panel to the tool window

--- src/com/seaniscool/freshbooks/FreshBooksToolWindowFactory.java
+++ src/com/seaniscool/freshbooks/FreshBooksToolWindowFactory.java
@@ -2,21 +2,32 @@ package com.seaniscool.freshbooks;
 
 import java.awt.*;
 
-import javax.swing.*;
-
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.seaniscool.freshbooks.ui.FreshBooksPanel;
 
 /**
+ * A {@link ToolWindow} factory.<br/>
+ * Lazy initializes the window when called.
+ *
  * @author Sean Connolly
  */
 public class FreshBooksToolWindowFactory implements ToolWindowFactory {
 
+	/**
+	 * Create the given {@code toolWindow}, adding a {@link FreshBooksPanel} to
+	 * it's contents.
+	 *
+	 * @param project
+	 *            the current active project
+	 * @param toolWindow
+	 *            the tool window which needs to be created
+	 */
 	@Override
 	public void createToolWindowContent(Project project, ToolWindow toolWindow) {
 		Component component = toolWindow.getComponent();
-		component.getParent().add(new JLabel("Hello, World!"));
+		component.getParent().add(new FreshBooksPanel());
 	}
 
 }
---
+++ src/com/seaniscool/freshbooks/ui/FreshBooksPanel.java
@@ -0,0 +1,14 @@
+package com.seaniscool.freshbooks.ui;
+
+import javax.swing.*;
+
+/**
+ * @author Sean Connolly
+ */
+public class FreshBooksPanel extends JPanel {
+
+	public FreshBooksPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+	}
+
+}
